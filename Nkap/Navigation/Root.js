63d93093ae836cb6b763976b6c370393097ad378
unknown <tanko.leila123@gmail.com>
2023-03-05 14:05:44 +0100

    Start Page register

--- Nkap/Navigation/Root.js
+++ Nkap/Navigation/Root.js
@@ -1,11 +1,13 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import Splash from '../Screen/Splash';
+import Inscription from '../Screen/Inscription';
 
 const stack = createStackNavigator()
 export default function Root(){
     return(
             <stack.Navigator initialRouteName="Splash">
                     <stack.Screen component={Splash} name = " ExpressNkap"/>
+                    <stack.Screen component={Inscription} name= "Register"/>
             </stack.Navigator>
     );
 }
---
+++ Nkap/Screen/Inscription.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text} from 'react-native';
+
+export default function Inscription(){
+    return(
+        <Text>Bonjour</Text>
+    );
+}

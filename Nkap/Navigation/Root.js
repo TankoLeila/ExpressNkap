94b3d688f5d1d5e5fcb1ef446cd66a64561a9947
Rabier <=>
2023-03-06 20:15:19 +0100

    Personnel

--- Nkap/Navigation/Root.js
+++ Nkap/Navigation/Root.js
@@ -2,14 +2,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Splash from '../Screen/Splash';
 import Inscription from '../Screen/Inscription';
 import Connection from "../Screen/connexion";
+import Personnel from "../Screen/Personnel";
 
 const stack = createStackNavigator()
 export default function Root(){
     return(
-            <stack.Navigator initialRouteName="Splash">
+            <stack.Navigator initialRouteName="Personnel">
                     <stack.Screen component={Splash} name = " ExpressNkap"/>
                     <stack.Screen component={Inscription} name= "Register"/>
                     <stack.Screen component={Connection} name="Connection"/>
+                    <stack.Screen component={Personnel} name="Personnel"/>
             </stack.Navigator>
     );
 }

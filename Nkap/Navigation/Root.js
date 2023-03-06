f8d28201d8c6036ca3cdfbad3eafbc6ebaec2428
peughouia <peughouia.44@gmail.com>
2023-03-06 09:12:06 +0100

    modification

--- Nkap/Navigation/Root.js
+++ Nkap/Navigation/Root.js
@@ -2,14 +2,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Splash from '../Screen/Splash';
 import Inscription from '../Screen/Inscription';
 import Connection from "../Screen/connexion";
+import Accueil from "../Screen/Accueil";
 
 const stack = createStackNavigator()
 export default function Root(){
     return(
-            <stack.Navigator initialRouteName="Splash">
+            <stack.Navigator initialRouteName="Accueil">
                     <stack.Screen component={Splash} name = " ExpressNkap"/>
                     <stack.Screen component={Inscription} name= "Register"/>
                     <stack.Screen component={Connection} name="Connection"/>
+                    <stack.Screen component={Accueil} name ="Accueil"/>
             </stack.Navigator>
     );
 }
--- Nkap/Screen/connexion.js
+++ Nkap/Screen/connexion.js
@@ -1,8 +1,20 @@
 import React from "react";
-import {Text} from 'react-native';
+import {Text,TouchableOpacity,View,StyleSheet} from 'react-native';
 
 export default function Connection(){
     return(
-        <Text>Bonjour</Text>
+       <View>
+        
+       </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#f10f2f5',
+      alignItems: 'left',
+      justifyContent: 'top',
+      paddingTop:48
+    }
+});
